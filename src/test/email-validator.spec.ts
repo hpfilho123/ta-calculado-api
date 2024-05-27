c1c27c8174fa742ad6fdc2cf78f46f253a3267c2
Hélio pereira <hpfilho123@gmail.com>
2024-05-27 01:52:21 -0300

    test: email validor adapter retorno true, false

--- src/test/email-validator.spec.ts
+++ src/test/email-validator.spec.ts
@@ -1,9 +1,17 @@
-import { EmailValidatorAdpter } from '@/utils/email-validator';
+import { EmailValidatorAdpter } from '@/utils/email-validator-adapter';
 
 describe('email validator Adapter', () => {
   test('deve retornar false se o validador retornar false ', () => {
     const sut = new EmailValidatorAdpter();
     const isValid = sut.isvalid('invalidEmail');
     expect(isValid).toBe(false);
+  });
+});
+
+describe('email validator Adapter', () => {
+  test('deve retornar true se o validador retornar true ', () => {
+    const sut = new EmailValidatorAdpter();
+    const isValid = sut.isvalid('hpsantos@bernhoeft.com.br');
+    expect(isValid).toBe(true);
   });
 });
--- src/utils/email-validator-adapter.ts
+++ src/utils/email-validator-adapter.ts
@@ -1,7 +1,8 @@
 import { EmailValidator } from '@/interfaces/email-validator.interface';
+import validator from 'validator';
 
 export class EmailValidatorAdpter implements EmailValidator {
   isvalid(email: string): boolean {
-    return false;
+    return validator.isEmail(email);
   }
 }
